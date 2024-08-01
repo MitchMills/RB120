12f4802bb1cc128e4aa1cc181f009be4ef2e3005
Mitch Mills <mitchmillsgcp@gmail.com>
2024-08-01 11:49:59 -0500

    RB120 Exercises

--- oo_book_exercises.rb
+++ oo_book_exercises.rb
@@ -1,10 +1,50 @@
-class Person
-  attr_accessor :name
+class MyCar
+  attr_accessor :color
+  attr_reader :year, :model
 
-  def initialize(name)
-    @name = name
+  def initialize(year, color, model)
+    @year = year
+    self.color = color
+    @model = model
+    @current_speed = 0
+  end
+
+  def accelerate(amount)
+    @current_speed += amount
+    puts "Accelerating by #{amount} miles per hour."
+    current_speed
+  end
+
+  def decelerate(amount)
+    @current_speed -= amount
+    puts "Decelerating by #{amount} miles per hour."
+    current_speed
+  end
+
+  def current_speed
+    puts "Your current speed is now #{@current_speed} miles per hour."
+  end
+
+  def turn_off
+    @current_speed = 0
+    puts "You have turned off the car."
+  end
+
+  def spray_paint(color)
+    former_color = self.color
+    self.color = color
+    puts "Your formerly #{former_color} car is now #{color}!"
+  end
+
+  def self.gas_mileage(miles_driven, gallons_used)
+    mpg = miles_driven.fdiv(gallons_used).round(2)
+    puts "Your gas mileage is #{mpg} miles per gallon."
+  end
+
+  def to_s
+    "Your car is a #{year} #{color} #{model}."
   end
 end
 
-bob = Person.new("Steve")
-bob.name = "Bob"
+fred = MyCar.new(1997, 'red', 'prizm')
+puts fred
