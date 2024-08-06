f5134f6a6203a392dfd0eafeb9e7778029d421f3
Mitch Mills <mitchmillsgcp@gmail.com>
2024-08-06 14:33:13 -0500

    RB120 Exercises

---
+++ oo_problems.rb
@@ -0,0 +1,35 @@
+### OO BASICS: CLASSES AND OBJECTS I
+## 05 Hello Sophie I
+# class Cat
+#   attr_accessor :name
+#   def initialize(name)
+#     self.name = name
+#     puts "Hello! My name is #{name}!"
+#   end
+# end
+
+# kitty = Cat.new('Sophie')
+
+## 04 What Are You?
+# class Cat
+#   def initialize
+#     puts "I'm a cat!"
+#   end
+# end
+
+# kitty = Cat.new
+
+## 03 Create an Object
+# class Cat
+# end
+
+# kitty = Cat.new
+
+## 02 Create the Class
+# class Cat
+# end
+
+## 01 Find the Class
+# puts "Hello".class
+# puts 5.class
+# puts [1, 2, 3].class
