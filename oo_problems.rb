ca4b0415e53f1b2486d5982d483a48d62095a247
Mitch Mills <mitchmillsgcp@gmail.com>
2024-08-07 16:56:57 -0500

    RB120 Exercises

--- oo_problems.rb
+++ oo_problems.rb
@@ -1,4 +1,102 @@
+### OO BASICS: CLASSES AND OBJECTS II
+## 01
+
 ### OO BASICS: CLASSES AND OBJECTS I
+## 10 Walk the Cat
+# module Walkable
+#   def walk
+#     puts "Let's go for a walk!"
+#   end
+# end
+
+# class Cat
+#   include Walkable
+
+#   attr_accessor :name
+
+#   def initialize(name)
+#     self.name = name
+#   end
+
+#   def greet
+#     puts "Hi! My name is #{name}!"
+#   end
+# end
+
+# kitty = Cat.new('Sophie')
+# kitty.greet
+# kitty.name = 'Luna'
+# kitty.greet
+# kitty.walk
+
+## 09 Accessor
+# class Cat
+#   attr_accessor :name
+
+#   def initialize(name)
+#     self.name = name
+#   end
+
+#   def greet
+#     puts "Hello! My name is #{name}!"
+#   end
+# end
+
+# kitty = Cat.new('Sophie')
+# kitty.greet
+# kitty.name = 'Luna'
+# kitty.greet
+
+## 08 Reader
+# class Cat
+#   attr_accessor :name
+
+#   def initialize(name)
+#     self.name = name
+#   end
+
+#   def greet
+#     puts "Hello! My name is #{name}!"
+#   end
+# end
+
+# kitty = Cat.new('Sophie')
+# kitty.greet
+# kitty.name = 'Luna'
+# kitty.greet
+
+## 07 Reader
+# class Cat
+#   attr_reader :name
+
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def greet
+#     puts "Hello! My name is #{name}!"
+#   end
+# end
+
+# kitty = Cat.new('Sophie')
+# kitty.greet
+# p kitty.name
+
+## 06 Hello Sophie II
+# class Cat
+#   attr_accessor :name
+#   def initialize(name)
+#     self.name = name
+#   end
+
+#   def greet
+#     puts "Hello! My name is #{name}!"
+#   end
+# end
+
+# kitty = Cat.new('Sophie')
+# kitty.greet
+
 ## 05 Hello Sophie I
 # class Cat
 #   attr_accessor :name
