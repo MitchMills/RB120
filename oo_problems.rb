a420cdf9aae41e04def4c435468c8f465bf5ff89
Mitch Mills <mitchmillsgcp@gmail.com>
2024-08-08 12:51:20 -0500

    RB120 Exercises

--- oo_problems.rb
+++ oo_problems.rb
@@ -1,5 +1,213 @@
+### OO BASICS: INHERITANCE
+## 10 Transportation
+# module Transportation
+#   class Vehicle
+#   end
+
+#   class Truck < Vehicle
+#   end
+
+#   class Car < Vehicle
+#   end
+# end
+
+## 09 Method Lookup III
+# module Flyable
+#   def fly
+#     "I'm flying"
+#   end
+# end
+
+# class Animal
+#   attr_reader :color
+
+#   def initialize(color)
+#     @color = color
+#   end
+# end
+
+# class Cat < Animal
+# end
+
+# class Bird < Animal
+#   include Flyable
+# end
+
+# bird1 = Bird.new('Red')
+# bird1.color
+
+# Bird
+# Flyable
+# Animal
+
+## 08 Method Lookup II
+# class Animal
+# end
+
+# class Cat < Animal
+# end
+
+# class Bird < Animal
+# end
+
+# cat1 = Cat.new
+# cat1.color
+
+# Cat
+# Animal
+# Object
+# Kernel
+# BasicObject
+
+## 07 Method Lookup I
+# class Animal
+#   attr_reader :color
+
+#   def initialize(color)
+#     @color = color
+#   end
+# end
+
+# class Cat < Animal
+# end
+
+# class Bird < Animal
+# end
+
+# cat1 = Cat.new('Black')
+# p cat1.color
+
+## 06 Towable II
+# module Towable
+#   def tow
+#     'I can tow a trailer!'
+#   end
+# end
+
+# class Vehicle
+#   attr_reader :year
+
+#   def initialize(year)
+#     @year = year
+#   end
+# end
+
+# class Truck < Vehicle
+#   include Towable
+# end
+
+# class Car < Vehicle
+# end
+
+# truck1 = Truck.new(1994)
+# puts truck1.year
+# puts truck1.tow
+
+# car1 = Car.new(2006)
+# puts car1.year
+
+## 05 Towable I
+# module Towable
+#   def tow
+#     puts "I can tow a trailer!"
+#   end
+# end
+
+# class Truck
+#   include Towable
+# end
+
+# class Car
+# end
 
 
+# truck1 = Truck.new
+# truck1.tow
+
+## 04 Start the Engine II
+# class Vehicle
+#   def start_engine
+#     'Ready to go!'
+#   end
+# end
+
+# class Truck < Vehicle
+#   def start_engine(speed)
+#     super() + " Drive #{speed}, please!"
+#   end
+# end
+
+# truck1 = Truck.new
+# puts truck1.start_engine('fast')
+
+## 03 Only Pass the Year
+# class Vehicle
+#   attr_reader :year
+
+#   def initialize(year)
+#     @year = year
+#   end
+# end
+
+# class Truck < Vehicle
+#   attr_reader :bed_type
+
+#   def initialize(year, bed_type)
+#     super(year)
+#     @bed_type = bed_type
+#   end
+# end
+
+# class Car < Vehicle
+# end
+
+# truck1 = Truck.new(1994, 'Short')
+# puts truck1.year
+# puts truck1.bed_type
+
+## 02 Start the Engine I
+# class Vehicle
+#   attr_reader :year
+
+#   def initialize(year)
+#     @year = year
+#   end
+# end
+
+# class Truck < Vehicle
+#   def initialize(year)
+#     super
+#     start_engine
+#   end
+
+#   def start_engine
+#     puts 'Ready to go!'
+#   end
+# end
+
+# truck1 = Truck.new(1994)
+# puts truck1.year
+
+## 01 Inherited Year
+# class Vehicle
+#   attr_reader :year
+
+#   def initialize(year)
+#     @year = year
+#   end
+# end
+
+# class Truck < Vehicle
+# end
+
+# class Car < Vehicle
+# end
+
+# truck1 = Truck.new(1994)
+# puts truck1.year
+
+# car1 = Car.new(2006)
+# puts car1.year
 
 ### OO BASICS: CLASSES AND OBJECTS II
 ## 10 PROTECTED SECRET
