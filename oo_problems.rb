facca740ed91730884839fb31690046a3a74a77a
Mitch Mills <mitchmillsgcp@gmail.com>
2024-08-13 11:51:37 -0500

    RB120 Exercises

--- oo_problems.rb
+++ oo_problems.rb
@@ -1,6 +1,106 @@
 ### OO EASY 2
-## 06
+## 07 Pet Shelter
+class Pet
+  attr_reader :type, :name
 
+  def initialize(type, name)
+    @type = type
+    @name = name
+  end
+end
+
+class Owner
+  attr_reader :name, :number_of_pets
+  attr_accessor :pets
+
+  def initialize(name)
+    @name = name
+    self.pets = []
+  end
+
+  def number_of_pets
+    pets.size
+  end
+
+  def to_s
+    name
+  end
+end
+
+class Shelter
+  attr_accessor :adoptions
+
+  def initialize
+    self.adoptions = {}
+  end
+
+  def adopt(owner, pet)
+    owner.pets << pet
+    adoptions[owner] ? adoptions[owner] << pet : adoptions[owner] = [pet]
+  end
+
+  def print_adoptions
+    adoptions.each do |owner, pets|
+      puts "#{owner} has adopted the following pets:"
+      pets.each do |pet|
+        puts "a #{pet.type} named #{pet.name}"
+      end
+      puts
+    end
+  end
+end
+
+butterscotch =  Pet.new('cat',            'Butterscotch')
+pudding =       Pet.new('cat',            'Pudding')
+darwin =        Pet.new('bearded dragon', 'Darwin')
+kennedy =       Pet.new('dog',            'Kennedy')
+sweetie =       Pet.new('parakeet',       'Sweetie Pie')
+molly =         Pet.new('dog',            'Molly')
+chester =       Pet.new('fish',           'Chester')
+
+phanson = Owner.new('P Hanson')
+bholmes = Owner.new('B Holmes')
+
+shelter = Shelter.new
+shelter.adopt(phanson, butterscotch)
+shelter.adopt(phanson, pudding)
+shelter.adopt(phanson, darwin)
+
+shelter.adopt(bholmes, kennedy)
+shelter.adopt(bholmes, sweetie)
+shelter.adopt(bholmes, molly)
+shelter.adopt(bholmes, chester)
+
+shelter.print_adoptions
+puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
+puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
+
+## 06 Comparing Wallets
+# class Wallet
+#   include Comparable
+
+#   def initialize(amount)
+#     @amount = amount
+#   end
+
+#   def <=>(other_wallet)
+#     amount <=> other_wallet.amount
+#   end
+
+#   protected
+#   attr_reader :amount
+# end
+
+# bills_wallet = Wallet.new(500)
+# pennys_wallet = Wallet.new(465)
+
+# if bills_wallet > pennys_wallet
+#   puts 'Bill has more money than Penny'
+# elsif bills_wallet < pennys_wallet
+#   puts 'Penny has more money than Bill'
+# else
+#   puts 'Bill and Penny have the same amount of money.'
+# end
 
 ## 05 What Will This Do?
 # class Something
