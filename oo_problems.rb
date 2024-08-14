fbaf19e85ba00c980717ff69a51c595ff4d29c55
Mitch Mills <mitchmillsgcp@gmail.com>
2024-08-14 17:54:37 -0500

    RB120 Exercises

--- oo_problems.rb
+++ oo_problems.rb
@@ -1,3 +1,14 @@
+# hash = {}
+# hash[:a] = 1
+# keys = [:a, :b, :c]
+# keys.each do |key|
+#   hash[key] ? nil : hash[key] = 3
+# end
+
+# p hash
+
+
+
 ### OO EASY 2
 ## 07 Pet Shelter
 =begin
@@ -23,109 +34,107 @@ Methods:
   shelter.print_unadopted
 =end
 
-class Pet
-  attr_reader :type, :name
+# class Pet
+#   attr_reader :type, :name
 
-  def initialize(type, name)
-    @type = type
-    @name = name
-  end
+#   def initialize(type, name)
+#     @type = type
+#     @name = name
+#   end
 
-  def to_s
-    "a #{type} named #{name}"
-  end
-end
+#   def to_s
+#     "a #{type} named #{name}"
+#   end
+# end
 
-class Owner
-  attr_reader :name, :pets
+# class Owner
+#   attr_reader :name, :pets
 
-  def initialize(name)
-    @name = name
-    @pets = []
-  end
+#   def initialize(name)
+#     @name = name
+#     @pets = {}
+#   end
 
-  def adopt_pet(pet)
-    @pets << pet
-  end
+#   def adopt_pet(pet)
+#     @pets[pet.name] = pet unless @pets[pet.name]
+#   end
 
-  def number_of_pets
-    pets.size
-  end
+#   def number_of_pets
+#     pets.size
+#   end
 
-  def print_pets
-    puts pets
-  end
-end
+#   def print_pets
+#     @pets.each { |name, pet| puts "  #{pet}" }
+#   end
+# end
 
-class Shelter
-  def initialize
-    @unadopted_pets = []
-    @adopters = {}
-  end
+# class Shelter
+#   def initialize
+#     @unadopted_pets = {}
+#     @adopters = {}
+#   end
 
-  def admit(pet)
-    @unadopted_pets << pet
-  end
+#   def admit(pet)
+#     @unadopted_pets[pet.name] = pet unless @unadopted_pets[pet.name]
+#   end
 
-  def number_of_pets
-    @unadopted_pets.size
-  end
+#   def number_of_pets
+#     @unadopted_pets.size
+#   end
 
-  def adopt(owner, pet)
-    @unadopted_pets - [pet]
-    owner.adopt_pet(pet)
-    @adopters[owner.name] = owner unless @adopters[owner.name]
-  end
+#   def adopt(owner, pet)
+#     @unadopted_pets.delete(pet.name)
+#     owner.adopt_pet(pet)
+#     @adopters[owner.name] = owner unless @adopters[owner.name]
+#   end
 
-  def print_adoptions
-    @adopters.each do |name, owner|
-      puts "#{name} has adopted the following pets:"
-      owner.print_pets
-      puts
-    end
-  end
+#   def print_adoptions
+#     @adopters.each do |name, owner|
+#       puts "#{name} has adopted the following #{owner.number_of_pets} pets:"
+#       owner.print_pets
+#       puts
+#     end
+#   end
 
-  def print_unadopted
-    puts "the Animal Shelter has the following unadopted pets:"
-    puts @unadopted_pets
-  end
-end
+#   def print_unadopted
+#     puts "the Animal Shelter has the following unadopted pets:"
+#     @unadopted_pets.each { |name,pet| puts "  #{pet}" }
+#   end
+# end
 
-butterscotch =  Pet.new('cat',            'Butterscotch')
-pudding =       Pet.new('cat',            'Pudding')
-darwin =        Pet.new('bearded dragon', 'Darwin')
-kennedy =       Pet.new('dog',            'Kennedy')
-sweetie =       Pet.new('parakeet',       'Sweetie Pie')
-molly =         Pet.new('dog',            'Molly')
-chester =       Pet.new('fish',           'Chester')
+# butterscotch =  Pet.new('cat',            'Butterscotch')
+# pudding =       Pet.new('cat',            'Pudding')
+# darwin =        Pet.new('bearded dragon', 'Darwin')
+# kennedy =       Pet.new('dog',            'Kennedy')
+# sweetie =       Pet.new('parakeet',       'Sweetie Pie')
+# molly =         Pet.new('dog',            'Molly')
+# chester =       Pet.new('fish',           'Chester')
 
-phanson = Owner.new('P Hanson')
-bholmes = Owner.new('B Holmes')
+# phanson = Owner.new('P Hanson')
+# bholmes = Owner.new('B Holmes')
 
-shelter = Shelter.new
-shelter.admit(butterscotch)
-shelter.admit(pudding)
-shelter.admit(darwin)
-shelter.admit(kennedy)
-shelter.admit(sweetie)
-shelter.admit(molly)
-shelter.admit(chester)
-shelter.print_unadopted
-puts "The Animal shelter has #{shelter.number_of_pets} unadopted pets."
-puts
+# shelter = Shelter.new
+# shelter.admit(butterscotch)
+# shelter.admit(pudding)
+# shelter.admit(darwin)
+# shelter.admit(kennedy)
+# shelter.admit(sweetie)
+# shelter.admit(molly)
+# shelter.admit(chester)
+# shelter.print_unadopted
+# puts "The Animal shelter has #{shelter.number_of_pets} unadopted pets."
+# puts
 
-shelter.adopt(phanson, butterscotch)
-shelter.adopt(phanson, pudding)
-shelter.adopt(phanson, darwin)
+# shelter.adopt(phanson, butterscotch)
+# shelter.adopt(phanson, pudding)
+# shelter.adopt(phanson, darwin)
 
-shelter.adopt(bholmes, kennedy)
-shelter.adopt(bholmes, sweetie)
-shelter.adopt(bholmes, molly)
-shelter.adopt(bholmes, chester)
+# shelter.adopt(bholmes, kennedy)
+# shelter.adopt(bholmes, sweetie)
+# shelter.adopt(bholmes, molly)
+# shelter.adopt(bholmes, chester)
 
-shelter.print_adoptions
-puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
-puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
+# shelter.print_adoptions
 
 ## 06 Comparing Wallets
 # class Wallet
