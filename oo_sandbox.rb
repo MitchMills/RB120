3bd7ef93b2b9fb84f230303c5fb74210c4c85845
Mitch Mills <mitchmillsgcp@gmail.com>
2024-08-18 17:52:58 -0500

    RB120 Exercises

--- oo_sandbox.rb
+++ oo_sandbox.rb
@@ -1,43 +1,14 @@
-class Animal
-  def speak(sound)
-    puts "#{sound}"
+class Vehicle
+  WHEELS = 4
+end
+
+WHEELS = 6
+
+class Car < Vehicle
+  def wheels
+    WHEELS
   end
 end
 
-class Horse < Animal
-  attr_reader :sound
-
-  def initialize
-    @sound = 'Neigh!'
-  end
-end
-
-class Cat < Animal
-  attr_reader :sound
-
-  def initialize
-    @sound = 'Meow!'
-  end
-end
-
-class Dog < Animal
-  attr_reader :sound
-
-  def initialize
-    @sound = 'Woof!'
-  end
-end
-
-class Bird < Animal
-  attr_reader :sound
-
-  def initialize
-    @sound = 'Tweet!'
-  end
-end
-
-all_animals = [Horse.new, Cat.new, Dog.new, Bird.new]
-
-all_animals.each do |animal|
-  animal.speak(animal.sound)
-end
+car = Car.new
+puts car.wheels        # => 4
