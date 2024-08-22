ef1bb6fc952ec7d764d50054ad86af29feeb6290
Mitch Mills <mitchmillsgcp@gmail.com>
2024-08-22 08:36:30 -0500

    RB120 Exercises

--- oo_sandbox.rb
+++ oo_sandbox.rb
@@ -1,12 +1,26 @@
-class Light
-  attr_accessor :brightness, :color
+class KrispyKreme
+  attr_reader :filling_type, :glazing
 
-  def initalize(brightness, color)
-    @brightness = brightness
-    @color = color
+  def initialize(filling_type, glazing)
+    @filling_type = filling_type
+    @glazing = glazing
   end
 
-  def self.information
-    return "I want to turn on the light with a brightness level of super high and a color of green"
+  def to_s
+    filling = filling_type ? "#{filling_type.capitalize}" : "Plain"
+    with_glazing = glazing ? " with #{glazing}" : ''
+    "#{filling}#{with_glazing}"
   end
 end
+
+donut1 = KrispyKreme.new(nil, nil)
+donut2 = KrispyKreme.new("Vanilla", nil)
+donut3 = KrispyKreme.new(nil, "sugar")
+donut4 = KrispyKreme.new(nil, "chocolate sprinkles")
+donut5 = KrispyKreme.new("Custard", "icing")
+
+puts donut1 # => "Plain"
+puts donut2 # => "Vanilla"
+puts donut3 # => "Plain with sugar"
+puts donut4 # => "Plain with chocolate sprinkles"
+puts donut5 # => "Custard with icing"
