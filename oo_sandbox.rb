8b359aac88beb37bd245a38068bcaaf0228f20b9
Mitch Mills <mitchmillsgcp@gmail.com>
2024-08-22 07:57:59 -0500

    RB120 Exercises

--- oo_sandbox.rb
+++ oo_sandbox.rb
@@ -1,7 +1,12 @@
-moves = {
-  rock: ,
-  scissors:,
-  paper:,
-  spock:,
-  lizard:
-}
+class Light
+  attr_accessor :brightness, :color
+
+  def initalize(brightness, color)
+    @brightness = brightness
+    @color = color
+  end
+
+  def self.information
+    return "I want to turn on the light with a brightness level of super high and a color of green"
+  end
+end
