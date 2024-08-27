bb5c21fc325b8b718b7aa121e7db8bf41e74df63
Mitch Mills <mitchmillsgcp@gmail.com>
2024-08-27 17:57:28 -0500

    Worked on tic tac toe bonus features

--- oo_ttt/tic_tac_toe2.rb
+++ oo_ttt/tic_tac_toe2.rb
@@ -5,3 +5,47 @@ end
 module TTTMove
 
 end
+
+module ComputerStrategy
+
+end
+
+
+
+class Player
+
+end
+
+class Human < Player
+
+end
+
+class Computer < Player
+
+end
+
+
+
+class Game
+
+end
+
+class SingleGame
+
+end
+
+class Match
+
+end
+
+
+
+class Round
+
+end
+
+
+
+class Engine
+
+end
