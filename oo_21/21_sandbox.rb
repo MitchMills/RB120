5c5d63fee0c2141ad9597e1edfcf99d9c422e5ac
Mitch Mills <mitchmillsgcp@gmail.com>
2024-09-07 16:27:09 -0500

    Worked on dealer turn

--- oo_21/21_sandbox.rb
+++ oo_21/21_sandbox.rb
@@ -1,9 +1,45 @@
 =begin
+
+
 *** EARLY FINISH / NO DEALER TURN ***
 
-player busted => DEALER WINS
-  {display message: busted
+PLAYER BLACKJACK && DEALER BLACKJACK
+  {display opening deal
+  {display opening hands => PLAYER BLACKJACK
+  {display message: blackjack
   {any key to continue
+
+  * clear screen *
+  [display message: dealer reveal
+  [display all cards => DEALER BLACKJACK
+  display message: dealer blackjack
+  display message: tie
+
+
+PLAYER BLACKJACK && NO DEALER BLACKJACK
+  {display opening deal
+  {display opening hands => PLAYER BLACKJACK
+  {display message: blackjack
+  {any key to continue
+
+  * clear screen *
+  [display message: dealer reveal
+  [display all cards => NO DEALER BLACKJACK
+  display message: not dealer blackjack
+  display message: you win
+
+
+PLAYER BUSTED
+  {display opening deal
+  {display opening hands => NO BLACKJACK
+  {display message: hit or stay => PLAYER HITS (can be multiple times)
+  * clear screen *
+  <display message: you chose to hit
+  <diplay card deal => PLAYER BUSTED
+  <display hands (hidden card)
+  {display message: player busted
+  {any key to continue
+
   * clear screen *
   [display message: dealer reveal
   [display all cards
@@ -11,41 +47,34 @@ player busted => DEALER WINS
   display message: dealer wins
 
 
-player blackjack && dealer blackjack => TIE
-  {display message: blackjack
-  {any key to continue
-  * clear screen *
-  [display message: dealer reveal
-  [display all cards
-  display message: dealer blackjack
-  display message: tie
-
-
-player blackjack && dealer not blackjack => PLAYER WINS
-  {display message: blackjack
-  {any key to continue
-  * clear screen *
-  [display message: dealer reveal
-  [display all cards
-  display message: no dealer blackjack
-  display message: you win
 
 
 
 *** REGULAR FINISH / PROCEED TO DEALER TURN
 
-player twenty one => COMPARE
-  {display message: twenty-one
+PLAYER TWENTY ONE
+  {display opening deal
+  {display opening hands => NO BLACKJACK
+  {display message: hit or stay => PLAYER HITS (can be multiple times)
+  * clear screen *
+  <display message: you chose to hit
+  <diplay card deal => PLAYER TWENTY ONE
+  <display hands (hidden card)
+  {display message: player twenty-one
   {any key to continue
+
   * clear screen *
   [display message: dealer reveal
   [display all cards
   DEALER TURN
 
-player stays => COMPARE
-  {display message: you stayed
-  {any key to continue
+PLAYER STAYS
+  {display opening deal
+  {display opening hands => NO BLACKJACK
+  {display message: hit or stay => PLAYER STAYS (can be after hits)
+
   * clear screen *
+  <display message: you chose to stay
   [display message: dealer reveal
   [display all cards
   DEALER TURN
