47bda75643ce810b31f6ac0e1364e8ef506574f3
Mitch Mills <mitchmillsgcp@gmail.com>
2024-09-17 06:38:36 -0500

    Review

--- oo_book_exercises.rb
+++ oo_book_exercises.rb
@@ -1,3 +1,5 @@
+
+
 ### QUESTION TWO
 =begin
 A module is a collection of behaviors (methods) that can be used by other classes if the module is included ("mixed in") in the class definition.
@@ -7,19 +9,37 @@ One purpose of a module is to group a set of behaviors / methods in one place so
 A module can be used by a class if the `#include` method is used in the definition of that class, with the module name as an argument.
 =end
 
-module Audible
-  def make_sound(sound)
-    puts sound
-  end
-end
+# module Audible
+#   def make_sound(sound)
+#     puts sound
+#   end
+# end
 
-class Cat
-  include Audible
-end
+# class Cat
+#   include Audible
+# end
 
-chumley = Cat.new
-chumley.make_sound('meow!')
+# chumley = Cat.new
+# chumley.make_sound('meow!')
 
+=begin
+A module is a collection of behaviors (methods) and/or constants. One purpose of modules is to enable multiple classes to access common constants and/or methods without having to duplicate them in each class.
+
+A module can be used with a class by invoking the `#include` method, followed by the module's name, within the definition of that class.
+=end
+
+# module Steerable
+#   def turn_left
+#     puts "Turning left"
+#   end
+# end
+
+# class Vehicle
+#   include Steerable
+# end
+
+# car = Vehicle.new
+# car.turn_left
 
 ### QUESTION ONE
 =begin
@@ -36,3 +56,12 @@ We create an object in Ruby by invoking the `#new` method on either a built-in c
 
 # puts my_string
 # puts my_object
+
+=begin
+We can create an object in Ruby by invoking the `#new` method on an existing class (whether built-in or custom). This instantiates a new instance of that class.
+=end
+
+# class Vehicle; end
+
+# car = Vehicle.new
+# p car
