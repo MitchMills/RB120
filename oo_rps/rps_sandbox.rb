a2bf27761630d3b9addee599d30c4203e1ba8651
Mitch Mills <mitchmillsgcp@gmail.com>
2024-09-21 06:53:40 -0500

    New rps implementation notes

--- oo_rps/rps_sandbox.rb
+++ oo_rps/rps_sandbox.rb
@@ -1,15 +1,35 @@
 =begin
-OBJECTS
+* OBJECTS *
 
 Player
-
+  @name
+  @choice
 Human < Player
 Computer < Player
 
 Choice
   CHOICES = [rock, paper, scissors, spock, lizard]
+  @choice
 
 Game
+  @human
+  @computer
+  @stats
+    total games played
+    matches stats
+    games stats
 Match
+  @scores = {player: 0, computer: 0, ties: 0}
+  @winner
 Round
+  @winner
+
+* ORCHESTRATION *
+intro
+set names
+match loop
+  round
+  match result
+result
+outro
 =end
